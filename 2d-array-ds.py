3b3d3b1bfe5c7c460c33168b62c79bdef64be0b6
Stefano Zullo <stefano.zullo.95@gmail.com>
2019-09-24 19:54:28 +0100

    2D Array - DS

---
+++ 2d-array-ds.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    max = -10000
+    for i in range(1, len(arr) - 1):
+        for j in range(1, len(arr[0]) - 1):
+            sum = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1] 
+            if sum > max:
+                max = sum
+    return max
+                
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
