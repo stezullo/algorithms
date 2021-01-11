71826762f64f51278dbd000dff622643eb5f4745
Stefano Zullo <stefano.zullo.95@gmail.com>
2021-01-11 22:01:43 +0100

    Create subrectangle-queries.js

---
+++ subrectangle-queries.js
@@ -0,0 +1,28 @@
+var SubrectangleQueries = function(rectangle) {
+    this._rectangle = rectangle;
+};
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2 
+ * @param {number} newValue
+ * @return {void}
+ */
+SubrectangleQueries.prototype.updateSubrectangle = function(row1, col1, row2, col2, newValue) {
+    for(let i = row1; i <= row2; i++) {
+        for(let j = col1; j <= col2; j++) {
+            this._rectangle[i][j] = newValue;
+        }
+    }
+};
+
+/** 
+ * @param {number} row 
+ * @param {number} col
+ * @return {number}
+ */
+SubrectangleQueries.prototype.getValue = function(row, col) {
+    return this._rectangle[row][col];
+};
