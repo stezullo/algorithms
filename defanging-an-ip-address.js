8cfbb2411ea0846174381ce9e48b9f021b709366
Stefano Zullo <stefano.zullo.95@gmail.com>
2021-05-29 19:49:04 +0200

    Update defanging-an-ip-address.js

--- defanging-an-ip-address.js
+++ defanging-an-ip-address.js
@@ -3,16 +3,5 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    const n = address.length;
-    
-    let str = "";
-    
-    if(n > 0) {    
-        for(let i = 0, val = ""; i < n; i++) {
-            val = address.charAt(i);
-            str += (val === '.') ? "[.]" : val;
-        }
-    }
-    
-    return str;
+   return address.split('.').join('[.]');
 };
